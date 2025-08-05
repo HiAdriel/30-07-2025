f5b081ba4fd2b07777dfa125fb6ceced75674e22
HiAdriel <adriel_tjahja_34rpl@student.smktelkom-mlg.sch.id>
2025-08-06 06:09:18 +0700

    Add files via upload
    RABU, 8 AGUSTUS 2025

---
+++ DataDiri_TemanSekelas.java
@@ -0,0 +1,103 @@
+public class DataDiri_TemanSekelas {
+    public static void main(String[] args) {
+        System.out.println("=== Data Identitas Diri Teman Sekelas ===");
+        // tide_data variabel = "nilai";
+        // "" hanya digunakan untuk variabel String
+        String firstname = "Grisli ";
+        String lastname = "Billywanto";
+        String fullname = firstname + lastname;
+        int usia = 16;
+        String kelas = "X RPL 7";
+        String smp = "SMP Negeri 4 Kepanjen";
+        String hobi = "Belajar";
+        String cita_cita = "Data analyst";
+        System.out.println("Nama : " + fullname);
+        System.out.println("Usia : " + usia + " Tahun");
+        System.out.println("Kelas : " + kelas);
+        System.out.println("Asal SMP : " + smp);
+        System.out.println("Hobi : " + hobi);
+        System.out.println("Cita-cita : " + cita_cita);
+
+        System.out.println(" ");
+        String firstname1 = "M Rohman ";
+        String lastname1 = "Muhammad";
+        String fullname1 = firstname1 + lastname1;
+        int usia1 = 15;
+        String kelas1 = "X RPL 7";
+        String smp1 = "SMP Negeri 2 Sumberpucung";
+        String hobi1 = "Running";
+        String cita_cita1 = "Web designer";
+        System.out.println("Nama : " + fullname1);
+        System.out.println("Usia : " + usia1 + " Tahun");
+        System.out.println("Kelas : " + kelas1);
+        System.out.println("Asal SMP : " + smp1);
+        System.out.println("Hobi : " + hobi1);
+        System.out.println("Cita-cita : " + cita_cita1);
+
+        System.out.println(" ");
+        String firstname2 = "M. Nasywan ";
+        String lastname2 = "Alfardan";
+        String fullname2 = firstname2 + lastname2;
+        int usia2 = 15;
+        String kelas2 = "X RPL 7";
+        String smp2 = "SMPIT Insan Permata";
+        String hobi2 = "Olahraga";
+        String cita_cita2 = "Pengusaha";
+        System.out.println("Nama : " + fullname2);
+        System.out.println("Usia : " + usia2 + " Tahun");
+        System.out.println("Kelas : " + kelas2);
+        System.out.println("Asal SMP : " + smp2);
+        System.out.println("Hobi : " + hobi2);
+        System.out.println("Cita-cita : " + cita_cita2);
+
+        System.out.println(" ");
+        String firstname3 = "Daffa ";
+        String lastname3 = "Al-Hakim";
+        String fullname3 = firstname3 + lastname3;
+        int usia3 = 15;
+        String kelas3 = "X RPL 7";
+        String smp3 = "SMP Negeri 2 Sumberpucung";
+        String hobi3 = "Running";
+        String cita_cita3 = "Web designer";
+        System.out.println("Nama : " + fullname3);
+        System.out.println("Usia : " + usia3 + " Tahun");
+        System.out.println("Kelas : " + kelas3);
+        System.out.println("Asal SMP : " + smp3);
+        System.out.println("Hobi : " + hobi3);
+        System.out.println("Cita-cita : " + cita_cita3);
+
+        System.out.println(" ");
+        String firstname4 = "Raphael";
+        String lastname4 = "Manurung";
+        String fullname4 = firstname4 + lastname4;
+        int usia4 = 15;
+        String kelas4 = "X RPL 7";
+        String smp4 = "SMPK Mardi WIyata Malang";
+        String hobi4 = "Play game";
+        String cita_cita4 = "Programmer";
+        System.out.println("Nama : " + fullname4);
+        System.out.println("Usia : " + usia4 + " Tahun");
+        System.out.println("Kelas : " + kelas4);
+        System.out.println("Asal SMP : " + smp4);
+        System.out.println("Hobi : " + hobi4);
+        System.out.println("Cita-cita : " + cita_cita4);
+
+        System.out.println("");
+        String firstname5 = "Beefan";
+        String lastname5 = "Adi Julian Pratama";
+        String fullname5 = firstname5 + lastname5;
+        int usia5 = 15;
+        String kelas5 = "X RPL 7";
+        String smp5 = "MTSN 3 Nganjuk";
+        String hobi5 = "Bermain Game dan Berolahraga";
+        String cita_cita5 = "Menjadi Milyarder Sukses";
+        System.out.println("Nama : " + fullname5);
+        System.out.println("Usia : " + usia5 + " Tahun");
+        System.out.println("Kelas : " + kelas5);
+        System.out.println("Asal SMP : " + smp5);
+        System.out.println("Hobi : " + hobi5);
+        System.out.println("Cita-cita : " + cita_cita5);
+
+        System.out.println("=== Akhir Data Identitas Diri Teman Sekelas ===");
+    }
+}
